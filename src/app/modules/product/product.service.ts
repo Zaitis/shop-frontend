d6698de4506e5f9b46cea4e6287530c3aba5626f
Zaitis <krzysztof@painm.pl>
2023-03-20 17:36:21 +0100

    getting data from backend

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from './model/poduct';
 
 @Injectable({
@@ -6,31 +8,9 @@ import { Product } from './model/poduct';
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getProducts(): Product[]{
-    return [
-      {
-        name: "Product1",
-        category: "Category1",
-        description: "Description product1",
-        price: 19.95,
-        currency: "PLN"
-      },
-      {
-        name: "Product2",
-        category: "Category2",
-        description: "Description product2",
-        price: 2.95,
-        currency: "PLN"
-      },
-      {
-        name: "Product3",
-        category: "Category3",
-        description: "Description product3",
-        price: 3.95,
-        currency: "PLN"
-      }
-    ]
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>("/api/products");
   }
 }
