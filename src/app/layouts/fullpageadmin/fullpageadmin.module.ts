56cb6d2e8b2079269c271b9acc1dc3bbb0eacbf9
Zaitis <krzysztof@painm.pl>
2023-03-21 10:00:24 +0100

    added basic layout to adminPage, added pagination to product component

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -4,6 +4,7 @@ import { FullpageadminComponent } from './fullpageadmin.component';
 import { AdminComponent } from 'src/app/modules/admin/admin.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'src/app/shared/material.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     CommonModule,
     RouterModule,
     FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class FullpageadminModule { }
