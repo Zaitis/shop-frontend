c1e70a063122576bb5cc4f09c2918d25c2431e4b
Zaitis <krzysztof@painm.pl>
2023-07-10 21:36:21 +0200

    Add profile view, repair login for user

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { HeaderService } from './header.service';
 import { CartIconService } from 'src/app/modules/common/service/cart-icon.service';
+import { JwtService } from 'src/app/modules/common/service/jwt.service';
 
 @Component({
   selector: 'app-header',
@@ -12,16 +13,20 @@ export class HeaderComponent implements OnInit {
 
   title="Pet Shop";
   cartProductCounter= "";
+  isLoggedin = false;
+
   constructor(
     private cookieService: CookieService,
     private headerService: HeaderService,
-    private cartIconService: CartIconService
+    private cartIconService: CartIconService,
+    private jwtService: JwtService
   ) { }
 
   ngOnInit(): void {
     this.getCountPorducts();
     this.cartIconService.subject
     .subscribe(counter => this.cartProductCounter =String(counter > 0 ? counter:""));
+    this.isLoggedin = this.jwtService.isLoggedIn();
   }
 
   getCountPorducts(){
